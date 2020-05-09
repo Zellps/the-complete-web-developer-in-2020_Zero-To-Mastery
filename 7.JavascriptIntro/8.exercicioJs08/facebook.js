592f5769db337a1a5cd1cd8a2d32aac0a555628a
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-05-08 22:17:30 -0300

    Update facebook.js

--- 7.JavascriptIntro/8.exercicioJs08/facebook.js
+++ 7.JavascriptIntro/8.exercicioJs08/facebook.js
@@ -5,34 +5,55 @@
 var database = [{
     username: "zell",
     password: "xucrute"
-}]
+},
+{
+    username: "tifa",
+    password: "1234"
+},
+{
+    username: "barret",
+    password: "1443"
+}
+]
 
 
 
 //newsfeed
-var newsFeed = [{   
+var newsFeed = [{
     username: "squall",
-   timeline: "sql123"
+    timeline: "sql123"
 },
-{   
+{
     username: "seifer",
-   timeline: "sfr123"
+    timeline: "sfr123"
 },
-{   
+{
     username: "rinoa",
-   timeline: "rna123"
+    timeline: "rna123"
 }
 ]
+
+
+function isUserValid(username, password) {
+    for (var i = 0; i < database.length; i++) {
+        if (database[i].username === username &&
+            database[i].password === password) {
+            return true;
+        } else {
+        }
+    } return false;
+}
+
+function signIn(username, password) {
+    if (isUserValid(username, password)) {
+        console.log(newsFeed)
+    } else {
+        alert("Login ou senha inválidos!")
+    }
+}
+
 
 var userNamePrompt = prompt("Qual o seu username?")
 var passwordPrompt = prompt("Qual a sua senha?")
 
-function signIn(user, pass) {
-    if ( user === database[0].username && pass === database[0].password) {
-        console.log(newsFeed)
-    } else {
-        alert ("Login ou senha incorretos")
-    }
-}
-
-signIn(userNamePrompt,passwordPrompt)
+signIn(userNamePrompt, passwordPrompt)
